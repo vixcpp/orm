127f6b90117f63044dc9438734ebbddfbb6377de
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-04-18 16:40:26 +0300

    feat(orm): add umbrella header

---
+++ include/vix/orm/all.hpp
@@ -0,0 +1,29 @@
+/**
+ *
+ *  @file orm/all.hpp
+ *  @author Gaspard Kirira
+ *
+ *  @brief Internal aggregation header for the Vix ORM module.
+ *
+ *  This file includes the full ORM surface, including entities,
+ *  mappers, repositories, query builders, unit of work, and
+ *  database compatibility helpers.
+ *
+ *  For most use cases, prefer:
+ *    #include <vix/orm.hpp>
+ *
+ *  Copyright 2025, Gaspard Kirira.
+ *  All rights reserved.
+ *  https://github.com/vixcpp/vix
+ *
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  Vix.cpp
+ */
+#ifndef VIX_ORM_ALL_HPP
+#define VIX_ORM_ALL_HPP
+
+#include <vix/orm/orm.hpp>
+
+#endif // VIX_ORM_ALL_HPP
